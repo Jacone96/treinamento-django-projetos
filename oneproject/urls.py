f26db6491c6f411e90b12f2466f8fd26b52cc51b
jacone96 <jaconewsl96@gmail.com>
2023-03-15 19:23:39 -0300

    urls,paths

--- oneproject/urls.py
+++ oneproject/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def my_view(request):
+    return HttpResponse('um teste')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
+
 ]
